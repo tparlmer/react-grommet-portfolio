cbd8b40389ee5a2c584f48c2582b42c7411df336
Thomas Parlmer <tparlmer@gmail.com>
2022-07-22 11:16:34 -0400

    added ui skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Collapsible,
+  Grommet,
+  Box,
+  Button,
+  Heading,
+  ResponsiveContext,
+  Layer,
+} from "grommet";
+import { FormClose, Notification } from "grommet-icons";
+import React, { useState } from "react";
 
 function App() {
+  const theme = {
+    global: {
+      colors: {
+        brand: "#FFFFFF",
+      },
+      border: {
+        color: "black",
+        size: "medium",
+        side: "all",
+      },
+      font: {
+        family: "Source Code Pro",
+        size: "20px",
+        height: "20px",
+      },
+    },
+  };
+
+  const AppBar = (props) => (
+    <Box
+      border={{
+        color: "black",
+        size: "medium",
+        side: "all",
+      }}
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="brand"
+      pad={{ left: "medium", right: "small", vertical: "small" }}
+      elevation="medium"
+      style={{ zIndex: "1" }}
+      {...props}
+    />
+  );
+
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} themeMode="dark" full>
+      <ResponsiveContext.Consumer>
+        {size => (
+        <Box fill>
+          <AppBar>
+            <Heading level="3" margin="none">
+              Click Over There For Content ... 👉
+            </Heading>
+            <Button
+              icon={<Notification />}
+              onClick={() => setShowSidebar(!showSidebar)}
+            />
+          </AppBar>
+          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+            <Box flex align="center" justify="center">
+              app body
+            </Box>
+            {(!showSidebar || size !== "small") ? (
+              <Collapsible direction="horizontal" open={showSidebar}>
+                <Box
+                  flex
+                  width="medium"
+                  background="light-2"
+                  elevation="small"
+                  align="center"
+                  justify="center"
+                >
+                  sidebar
+                </Box>
+              </Collapsible>
+            ): (
+              <Layer>
+                <Box
+                  background='light-2'
+                  tag='header'
+                  justify='end'
+                  align='center'
+                  direction='row'
+                >
+                  <Button 
+                    icon={<FormClose />}
+                    onClick={() => setShowSidebar(false)}
+                  />
+                </Box>
+                <Box
+                  fill
+                  background='light-2'
+                  align='center'
+                  justify='center'
+                >
+                  sidebar
+                </Box>
+              </Layer>
+            )}
+          </Box>
+        </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// THIS IS WHERE THE ROUTER GETS EXPORTED
+import ReactDom from 'react-dom/client'
+import React from 'react';
+import BrowserRouter from 'react-router-dom';
+
+function Router() {
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Router;
